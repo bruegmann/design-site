963b4b4c5978ced90752f5361cd8b0b89d081dc2
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-10-26 12:32:41 +0100

    Changed to public proxy

--- src/shared.ts
+++ src/shared.ts
@@ -33,8 +33,11 @@ export function getPhrase(keyword: string, countryCode: string | undefined = und
     }
 }
 
-export const gitHubOauthUrl = "http://localhost:4000/login/oauth/access_token"
-export const proxy = "http://localhost:4000/fetch/"
+// export const gitHubOauthUrl = "http://localhost:4000/login/oauth/access_token"
+// export const proxy = "http://localhost:4000/fetch/"
+
+export const gitHubOauthUrl = "https://github-proxy.patorg.de/login/oauth/access_token"
+export const proxy = "https://github-proxy.patorg.de/fetch/"
 
 // In production (bruegmann.github.io) we need another client ID then in development (localhost:3000)
 export const gitHubOauthClientId = (!process.env.NODE_ENV || process.env.NODE_ENV === "development") ? "d64aa2c0928af6b72a90" : "0e88710af11826fb210a"
